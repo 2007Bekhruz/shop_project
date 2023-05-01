247646bd01e62966034de1fa2fab8b34437394b8
unknown <PROWEB-Aybek@1-9.intranet.proweb.uz>
2023-05-01 20:59:29 +0500

    added pages

--- pages/urls.py
+++ pages/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.home_view, name="home"),
     path("shop/", views.shop_view, name="shop"),
+    path("shop/categories/<slug:slug>/", views.category_products, name="category_products"),
+    path("shop/product/<slug:slug>/", views.product_detail, name="product_detail"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, HttpResponse
-from .models import Product
-# Create your views here.
+from .models import Product, Category
 
+
+# Create your views here.
 
 def home_view(request):
     return render(request, "pages/index.html")
@@ -13,3 +14,20 @@ def shop_view(request):
         "products": products
     }
     return render(request, 'pages/shop.html', context)
+
+
+def category_products(request, slug):
+    category = Category.objects.get(slug=slug)
+    products = Product.objects.filter(category=category)
+    context = {
+        "products": products
+    }
+    return render(request, 'pages/shop.html', context)
+
+
+def product_detail(request, slug):
+    product = Product.objects.get(slug=slug)
+    context = {
+        'product': product
+    }
+    return render(request, 'pages/product_detail.html', context)
